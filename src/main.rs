1d9b992b731b0a8b74e755959649c05fecc72dfd
mattDev0 <amatthew642@gmail.com>
2023-09-09 18:23:00 +0100

    re-organize

--- src/main.rs
+++ src/main.rs
@@ -20,6 +20,25 @@ struct Response {
     status_code: u16,
 }
 
+impl Response {
+    fn new(slack_name: String, track: String) -> Response {
+        let utc_time = Utc::now().format("%Y-%m-%dT%H:%M:%SZ").to_string();
+        let current_day = Utc::now().format("%A").to_string();
+        let github_repo_url = "https://github.com/mattDev0/endpoint_with_rust".to_string();
+        let github_file_url = format!("{}/blob/master/main.rs", github_repo_url);
+        let status_code = StatusCode::OK.as_u16();
+        Response {
+            slack_name,
+            current_day,
+            utc_time,
+            track,
+            github_file_url,
+            github_repo_url,
+            status_code,
+        }
+    }
+}
+
 #[tokio::main]
 async fn main() {
     let api = warp::path!("api")
@@ -27,21 +46,8 @@ async fn main() {
         .and(warp::query::<QueryParams>())
         .map(|params: QueryParams| {
             let slack_name = params.slack_name.unwrap_or_else(|| "Unknown".to_string());
-            let current_day = Utc::now().format("%A").to_string();
-            let utc_time = Utc::now().to_rfc3339_opts(chrono::SecondsFormat::Secs, true);
             let track = params.track.unwrap_or_else(|| "Unknown".to_string());
-            let github_repo_url = "https://github.com/mattDev0/endpoint_with_rust".to_string();
-            let github_file_url = format!("{}/blob/master/src/main.rs", github_repo_url);
-            let status_code = StatusCode::OK.as_u16();
-            let response = Response {
-                slack_name,
-                current_day,
-                utc_time,
-                track,
-                github_file_url,
-                github_repo_url,
-                status_code,
-            };
+            let response = Response::new(slack_name, track);
             let pretty_json = to_string_pretty(&response).unwrap();
             warp::reply::with_status(pretty_json, StatusCode::OK)
         });
